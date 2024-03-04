6385e8e9e57ed10dd69f80971130692531dd58a4
Mohammed Ahsan <earth.ahsan@gmail.com>
2024-03-04 17:43:29 +0600

    fixes

--- pages/components/src/ExperienceCard.tsx
+++ pages/components/src/ExperienceCard.tsx
@@ -7,7 +7,7 @@ type Props = {}
 function ExperienceCard({}: Props) {
   return (
     <div className='flex flex-col rounded-lg items-center space-y-3 flex-shrink-0 w-[350px]  md:w-[450px] xl:w-[600px] snap-center p-5 bg-gray-800 hover:opacity-100 opacity-60 cursor-pointer transition-opacity duration-200 overflow-hidden'>
-        <motion.img className='rounded-full bg-gray-50 w-32 h-32 xl:w-[200px] xl:h-[200px] object-cover object-center' alt="profile.gif"  src="https://i.ibb.co/2t2r9R6/icon.png" />
+        <motion.img className='rounded-full bg-gray-50 w-32 h-32 xl:w-[200px] xl:h-[200px] object-cover object-center' alt="profile.gif"  src="https://i.ibb.co/yXVPH4Q/spark-mesh-logo.jpg" />
         <div className='flex flex-row space-x-2'>
        
         <div>Software Engineer</div></div>
